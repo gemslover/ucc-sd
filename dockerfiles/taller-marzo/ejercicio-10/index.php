743ca695672ea04d9a6b142b006e2b87491b1b74
gemslover <julianalfredolozanosanabria@gmail.com>
2024-03-23 17:07:07 -0500

    Index en PHP

---
+++ dockerfiles/taller-marzo/ejercicio-10/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP App</title>
+</head>
+<body>
+    <h1>Hola desde PHP</h1>
+    <p>Este es un trabajo de sistemas distribuidos</p>
+    <?php
+    echo "¡Hola, mundo!";
+    ?>
+</body>
+</html>

f9cd3181141118a6f7047e3ecbc81ef9b817f6ab
gemslover <julianalfredolozanosanabria@gmail.com>
2024-03-23 17:08:19 -0500

    app json

---
+++ dockerfiles/taller-marzo/ejercicio-09/src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>¡Hola, mundo desde React!</h1>
+            <p>Esta es una aplicación de React ejecutándose en un contenedor Docker.</p>
+        </div>
+    );
+};
+
+export default App;
